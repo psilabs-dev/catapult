ea7a00827377dd667035ead039b979e53b89b08e
psilabs-dev <113860476+psilabs-dev@users.noreply.github.com>
2024-10-25 07:22:06 -0400

    close sql connection when done

--- src/catapult/cache.py
+++ src/catapult/cache.py
@@ -1,3 +1,4 @@
+from contextlib import closing
 import hashlib
 import logging
 from pathlib import Path
@@ -15,17 +16,17 @@ def create_cache_table():
     """
     Create archive_hash table, which maps the archive to its corresponding LRR ID.
     """
-    conn = sqlite3.connect(config.CATAPULT_CACHE_DB)
-    c = conn.cursor()
-    c.execute(
+    with closing(sqlite3.connect(config.CATAPULT_CACHE_DB)) as conn:
+        with closing(conn.cursor()) as c:
+            c.execute(
+                '''
+        CREATE TABLE IF NOT EXISTS archive_hash (
+        md5 VARCHAR(255) PRIMARY KEY,
+        archive_id VARCHAR(255)
+        )
         '''
-CREATE TABLE IF NOT EXISTS archive_hash (
-md5 VARCHAR(255) PRIMARY KEY,
-archive_id VARCHAR(255)
-)
-'''
-    )
-    conn.commit()
+            )
+        conn.commit()
     return
 
 def get_cached_archive_id_else_compute(archive_path: str) -> str:
@@ -33,14 +34,14 @@ def get_cached_archive_id_else_compute(archive_path: str) -> str:
     Get archive ID from cache if it exists (quick), otherwise compute it (slow) and add it to cache.
     """
     md5 = __calculate_md5_from_filepath(archive_path)
-    conn = sqlite3.connect(config.CATAPULT_CACHE_DB)
-    c = conn.cursor()
-    c.execute(
-        '''
-SELECT archive_id FROM archive_hash WHERE md5 = ?
-''', (md5, )
-    )
-    result = c.fetchone()
+    with closing(sqlite3.connect(config.CATAPULT_CACHE_DB)) as conn:
+        with closing(conn.cursor()) as c:
+            c.execute(
+                '''
+        SELECT archive_id FROM archive_hash WHERE md5 = ?
+        ''', (md5, )
+            )
+            result = c.fetchone()
 
     if result:
         return result[0]
@@ -49,16 +50,17 @@ SELECT archive_id FROM archive_hash WHERE md5 = ?
     archive_id = lrr_compute_id(archive_path)
     filename = Path(archive_path).name
 
-    c = conn.cursor()
-    c.execute('''INSERT OR IGNORE INTO archive_hash VALUES (?, ?)''', (md5, archive_id))
-    conn.commit()
+    with closing(sqlite3.connect(config.CATAPULT_CACHE_DB)) as conn:
+        with closing(conn.cursor()) as c:
+            c.execute('''INSERT OR IGNORE INTO archive_hash VALUES (?, ?)''', (md5, archive_id))
+        conn.commit()
     return archive_id
 
 def clear_cache():
-    conn = sqlite3.connect(config.CATAPULT_CACHE_DB)
-    c = conn.cursor()
-    c.execute('DELETE FROM archive_hash')
-    conn.commit()
+    with closing(sqlite3.connect(config.CATAPULT_CACHE_DB)) as conn:
+        with closing(conn.cursor()) as c:
+            c.execute('DELETE FROM archive_hash')
+        conn.commit()
     return
 
 create_cache_table()
