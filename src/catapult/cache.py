d3775aa6ef1a5b045d0e498ace3da64ebb99a2c0
psilabs-dev <113860476+psilabs-dev@users.noreply.github.com>
2024-11-21 00:48:26 -0500

    rename db to conn

--- src/catapult/cache.py
+++ src/catapult/cache.py
@@ -11,20 +11,20 @@ async def create_cache_table():
     Create `archive_hash` table which stores the MD5 hashes of the filenames of all 
     Archives that are uploaded to the Server.
     """
-    async with aiosqlite.connect(config.CATAPULT_CACHE_DB) as db:
-        await db.execute('''
+    async with aiosqlite.connect(config.CATAPULT_CACHE_DB) as conn:
+        await conn.execute('''
         CREATE TABLE IF NOT EXISTS archive_hash (
         md5 VARCHAR(255) PRIMARY KEY
         )
         ''')
-        await db.commit()
+        await conn.commit()
     return
 
 async def archive_hash_exists(archive_md5: str) -> bool:
     """
     Returns True if hash exists in cache, else False.
     """
-    async with aiosqlite.connect(config.CATAPULT_CACHE_DB) as db, db.execute('SELECT * FROM archive_hash WHERE md5 = ?', (archive_md5,)) as cursor:
+    async with aiosqlite.connect(config.CATAPULT_CACHE_DB) as conn, conn.execute('SELECT * FROM archive_hash WHERE md5 = ?', (archive_md5,)) as cursor:
             return await cursor.fetchone() is not None
 
 async def insert_archive_hash(archive_md5: str):
@@ -36,9 +36,9 @@ async def insert_archive_hash(archive_md5: str):
     retry_count = 0
     while True:
         try:
-            async with aiosqlite.connect(config.CATAPULT_CACHE_DB) as db:
-                await db.execute('INSERT OR IGNORE INTO archive_hash VALUES (?)', (archive_md5,))
-                await db.commit()
+            async with aiosqlite.connect(config.CATAPULT_CACHE_DB) as conn:
+                await conn.execute('INSERT OR IGNORE INTO archive_hash VALUES (?)', (archive_md5,))
+                await conn.commit()
                 return
         except aiosqlite.core.sqlite3.OperationalError:
             time_to_sleep = 2 ** (retry_count + 1)
@@ -46,9 +46,9 @@ async def insert_archive_hash(archive_md5: str):
             continue
 
 async def drop_cache_table():
-    async with aiosqlite.connect(config.CATAPULT_CACHE_DB) as db:
-        await db.execute('DROP TABLE IF EXISTS archive_hash')
-        await db.commit()
+    async with aiosqlite.connect(config.CATAPULT_CACHE_DB) as conn:
+        await conn.execute('DROP TABLE IF EXISTS archive_hash')
+        await conn.commit()
     return
 
 asyncio.run(create_cache_table())
